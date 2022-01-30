e63b0ed75649b2c16b65ea15462897db430cb115
captain-crypto <tamirf@pai-tech.org>
2022-01-30 17:06:09 +0200

    base folders

--- PAIOS.py
+++ PAIOS.py
@@ -27,10 +27,15 @@ class PAIOSInstaller:
             print("folder " + folder + " already exists, skipping")
 
     def create_pai_sub_folders(self,folders_arr):
+        self.create_sub_folders_for_base(folders_arr,self.pai_folder)
+    
+    def create_sub_folders_for_base(self,folders_arr,base_folder):
+        if not base_folder.endswith(os.path.sep):
+            base_folder = base_folder + os.path.sep
         for folder in folders_arr:
             #checkin for uppercase folder name, if lower fix
             if folder[0:1].islower():
                 folder = folder[0:1].upper() + folder[1:]
-            full_path = self.pai_folder + folder
+            full_path = base_folder + folder
             self.make_dir(full_path)
 
