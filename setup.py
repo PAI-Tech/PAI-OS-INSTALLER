ac5e61d918ccdfa8c3cca25af5f87df0e1a93ea7
caba-crypto <eran@pai-tech.org>
2022-01-30 19:10:53 +0200

    Folders-added

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
 from PAIOS import PAIOSInstaller 
 vp = "/var/PAI/"
-vp_folders=["Bots","Apps","Logs","backup"]
-bots_folder = ["Netbot","MasterBot"]
+vp_folders=["Apps","Backup","Bots","Cache","Config","Logs","Systems"]
+apps_folder = ["Dusty"]
+backup_folder = ["PAI-DDB"]
+bots_folder = []
+cache_folder = ["PAI-BOT"]
+config_folder = []
+logs_folder = ["PAI-Monitor"]
+system_folder = ["Scripts"]
 
 poi = PAIOSInstaller(vp)
 
 
 poi.create_pai_sub_folders(vp_folders)
+poi.create_sub_folders_for_base(apps_folder,vp + "Apps")
+poi.create_sub_folders_for_base(backup_folder,vp + "Backup")
 poi.create_sub_folders_for_base(bots_folder,vp + "Bots")
+poi.create_sub_folders_for_base(cache_folder,vp + "Cache")
+poi.create_sub_folders_for_base(config_folder,vp + "Config")
+poi.create_sub_folders_for_base(logs_folder,vp + "Logs")
+poi.create_sub_folders_for_base(system_folder,vp + "System")
 #poi.make_dir(vp + "Bots/netbot")
+
+"Event-Man-Bot","NET-BOT","Master-Bot","Maestro-Docker","PAI-Prompt","PAI-BU","PAI-Monitor","PAI-Restore"
